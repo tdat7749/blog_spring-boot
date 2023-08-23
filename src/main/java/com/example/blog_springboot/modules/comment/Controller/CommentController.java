3eaab9125cf3017e8f3accce1f5c2f970d0a5325
tdat7749 <hyperiondat@gmail.com>
2023-08-23 16:30:26 +0700

    fix sync docker

--- src/main/java/com/example/blog_springboot/modules/comment/Controller/CommentController.java
+++ src/main/java/com/example/blog_springboot/modules/comment/Controller/CommentController.java
@@ -1,12 +1,25 @@
 package com.example.blog_springboot.modules.comment.Controller;
 
 
-import org.springframework.stereotype.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController(value = "/comment")
 public class CommentController {
 
+    private static final Logger logger = LoggerFactory.getLogger(CommentController.class);
     public CommentController(){
 
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String abc(){
+        logger.info("Logger comment");
+        return "ngu";
     }
 }
