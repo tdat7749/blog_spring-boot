c869a29fa70c8f7b50bfcde5bea1f376ef2bcb7c
tdat7749 <hyperiondat@gmail.com>
2023-09-20 21:12:22 +0700

    add table: Notifications, user_notification, like_post for 2 modules

---
+++ src/main/java/com/example/blog_springboot/modules/likepost/model/LikePost.java
@@ -0,0 +1,35 @@
+package com.example.blog_springboot.modules.likepost.model;
+
+import com.example.blog_springboot.modules.post.Model.Post;
+import com.example.blog_springboot.modules.user.model.User;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "like_post")
+public class LikePost {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id",nullable = false)
+    @JsonManagedReference
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id",nullable = false)
+    @JsonManagedReference
+    private Post post;
+
+    @Column(name = "created_at",nullable = false)
+    private Date createdAt;
+
+    @Column(name = "updated_at",nullable = false)
+    private Date updatedAt;
+
+}
