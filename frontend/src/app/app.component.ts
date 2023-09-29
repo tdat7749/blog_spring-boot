b44c7c70d08d2c59159aac7c9ca2a2b02689c3a7
tdat7749 <hyperiondat@gmail.com>
2023-09-29 11:10:52 +0700

    config docker for angular

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'nhân sinh hỗn loạn alo 123 nghe n';
+  title = 'bố thăng docker rách';
 }
