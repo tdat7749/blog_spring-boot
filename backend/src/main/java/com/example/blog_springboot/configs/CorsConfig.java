683bb093963d44857621a71c17b52c83482ef8d7
tdat7749 <hyperiondat@gmail.com>
2023-10-10 16:09:04 +0700

    config cors for application

---
+++ backend/src/main/java/com/example/blog_springboot/configs/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.example.blog_springboot.configs;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+public class CorsConfig implements CorsConfigurationSource {
+
+
+    @Override
+    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        return config;
+    }
+}
